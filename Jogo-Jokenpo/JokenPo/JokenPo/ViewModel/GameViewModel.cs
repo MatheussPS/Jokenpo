df8adb4b1202cb20b1ae2d6d1aa37181815812c9
Matheus Pinter <matheuspinter08@gmail.com>
2024-06-20 19:56:05 -0300

    Feat: Projeto finalizado

--- Jogo-Jokenpo/JokenPo/JokenPo/ViewModel/GameViewModel.cs
+++ Jogo-Jokenpo/JokenPo/JokenPo/ViewModel/GameViewModel.cs
@@ -24,72 +24,145 @@ namespace JokenPo.ViewModel
         private String nome_Player1;
 
         [ObservableProperty]
+        private String nome_Player2;
+
+        [ObservableProperty]
         private int pontuacao_Player1;
 
         [ObservableProperty]
         private int pontuacao_Player2;
 
         [ObservableProperty]
-        private Escolha escolha_Player1;
+        private String escolha_Player1;
 
         [ObservableProperty]
         private String resultado;
 
         public ICommand JogarCommand { get; }
+        public ICommand ResetarCommand { get; }
 
         Player player1 = new Player();
+        Player player2 = new Player();
+
 
         public GameViewModel()
         {
             JogarCommand = new Command(Jogar);
+            ResetarCommand = new Command(Reseta);
+            resultado = "Joguem!";
+            Nome_Player1 = "Você";
+            Nome_Player2 = "Oponente";
+            Imagem_Player1 = "game.png";
+            Imagem_Player2 = "game.png";
+            Escolha_Player1 = "Pedra";
         }
 
         public void Jogar()
         {
 
-            Player player2 = new Player();
+            player1.Nome = Nome_Player1;
+            player2.Nome = "Inimigo";
+
+            Escolha Escolha_Player = ConvertToEscolha(Escolha_Player1);
+            player1.Escolha = Escolha_Player;
+            player2.Jogar();
+
+            AtualizaImagens();
+            AtualizaJogo();
+            AtualizaPontos();
+            VerificaPontos();
+        }
+
+        public void AtualizaPontos()
+        {
+            Pontuacao_Player1 = player1.Pontuacao;
+            Pontuacao_Player2 = player2.Pontuacao;
+
+        }
+
+        public void AtualizaPontos(Player jogador)
+        {
+            jogador.Pontuacao += 1;
+
+        }
+
+        public void AtualizaImagens()
+        {
+            Imagem_Player1 = $"escolha{((int)player1.Escolha)}.png";
+            Imagem_Player2 = $"escolha{((int)player2.Escolha)}.png";
+        }
+
+        public void Reseta()
+        {
 
             player1.Nome = Nome_Player1;
             player2.Nome = "Inimigo";
 
-            player1.Escolha = Escolha_Player1;
-            player2.Jogar();
+            player1.Pontuacao = 0;
+            player2.Pontuacao = 0;
 
-            atualizaImagens(player1, player2);
+            AtualizaPontos();
 
+            Imagem_Player1 = "game.png";
+            Imagem_Player2 = "game.png";
+            Resultado = "Joguem!";
+        }
+
+        public void AtualizaJogo()
+        {
             if (player1.Escolha == player2.Escolha)
             {
-                Resultado = "Empatou";
+                Resultado = $"Empatou";
             }
-            else if(player1.Escolha == Escolha.Pedra && player2.Escolha == Escolha.Tesoura 
+            else if (player1.Escolha == Escolha.Pedra && player2.Escolha == Escolha.Tesoura
                 || player1.Escolha == Escolha.Papel && player2.Escolha == Escolha.Pedra
                 || player1.Escolha == Escolha.Tesoura && player2.Escolha == Escolha.Papel)
             {
-                Resultado = $"{player1.Nome} venceu!";
-                atualizaPontos(player1);
+                Resultado = $"{player1.Nome} venceu a partida!";
+                AtualizaPontos(player1);
             }
             else
             {
-                Resultado = $"{player2.Nome} venceu!";
-                atualizaPontos(player2);
+                Resultado = $"{player2.Nome} venceu a partida!";
+                AtualizaPontos(player2);
             }
         }
 
-        public void atualizaPontos(Player jogador)
+        public async void VerificaPontos()
         {
-            jogador.Pontuacao += 1;
+
+            if (player1.Pontuacao == 10)
+            {
+                Resultado = $"{player1.Nome} venceu a rodada! O jogo será reiniciado em 5s";
+                await Task.Delay(5000);
+                Reseta();
+            }
+            else if (player2.Pontuacao == 10)
+            {
+                Resultado = $"{player2.Nome} venceu a rodada! O jogo será reiniciado em 5s";
+                await Task.Delay(5000);
+                Reseta();
+
+            }
+
+
         }
 
-        public void atualizaImagens(Player jogador1, Player jogador2)
+        public static Escolha ConvertToEscolha(String palavra)
         {
-            Imagem_Player1 = $"escolha{jogador1.Escolha}";
-            Imagem_Player2 = $"escolha{jogador2.Escolha}";
-        }
-
-        public void reseta()
-        {
-           
+            switch (palavra)
+            {
+                case "Pedra":
+                    return Escolha.Pedra;
+                case "Papel":
+                    return Escolha.Papel;
+                case "Tesoura":
+                    return Escolha.Tesoura;
+                default:
+                    throw new ArgumentException("Valor não reconhecido");
+            }
         }
 
     }
 }
+
